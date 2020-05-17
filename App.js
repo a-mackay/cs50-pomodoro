1cf7ae0dd4ee4f9734036b2a15cd11007088a92c
a-mackay <amk@gegroup.com.au>
2020-05-17 16:04:47 +1000

    Add some ugly code to get functionality working

--- App.js
+++ App.js
@@ -1,15 +1,134 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import vibrate from './utils/vibrate.js';
+
+const LARGE_SECONDS = 5; //25 * 60;
+const SMALL_SECONDS = 3;//5 * 60;
+const VIBRATING_SECONDS = 5;
+
+function getSeconds(timerType) {
+  if (timerType === "VIBRATING") {
+    return VIBRATING_SECONDS;
+  } else if (timerType === "SMALL") {
+    return SMALL_SECONDS;
+  } else {
+    return LARGE_SECONDS;
+  }
+}
+
+function getNextTimerType(timerType) {
+  if (timerType === "LARGE") {
+    return "SMALL";
+  } else if (timerType === "SMALL") {
+    return "LARGE";
+  }
+}
+
+function initialState() {
+  return {
+    nextTimerType: "SMALL", // SMALL or LARGE
+    timerType: "LARGE", // SMALL, LARGE, or VIBRATING
+    seconds: getSeconds("LARGE"),
+  }
+}
 
 export default function App() {
-  const seconds = 124;
+  const [timer, setTimer] = useState(initialState());
+  const [intervalId, setIntervalId] = useState(null);
+
+  const tickTimer = (previousTimer) => {
+    const secs = previousTimer.seconds;
+    const currentTimerType = previousTimer.timerType;
+
+    if (currentTimerType === "VIBRATING") {
+      vibrate();
+    }
+    if (secs === 0) {
+      if (currentTimerType === "VIBRATING") {
+        return {
+          nextTimerType: getNextTimerType(previousTimer.nextTimerType),
+          timerType: previousTimer.nextTimerType,
+          seconds: getSeconds(previousTimer.nextTimerType),
+        }
+      } else {
+        return {
+          nextTimerType: previousTimer.nextTimerType,
+          timerType: "VIBRATING",
+          seconds: getSeconds("VIBRATING"),
+        }
+      }
+    } else {
+      return {
+        timerType: previousTimer.timerType,
+        nextTimerType: previousTimer.nextTimerType,
+        seconds: previousTimer.seconds - 1,
+      }
+    }
+  }
+
+  const startTick = () => {
+    if (intervalId !== null) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+    const tempIntervalId = setInterval(() => setTimer(prevTimer => tickTimer(prevTimer)), 1000);
+    setIntervalId(tempIntervalId);
+  }
+
+  const stopTick = () => {
+    if (intervalId !== null) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+  }
+
+  const resetTimer = () => {
+    if (intervalId !== null) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+    setTimer(initialState())
+  }
+
+  const displaySeconds = () => {
+    if (timer.timerType === "VIBRATING") {
+      return 0;
+    } else {
+      return timer.seconds;
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>testing out first line of native</Text>
-      <TimeDisplay seconds={seconds}></TimeDisplay>
+      <TimeDisplay seconds={displaySeconds()}></TimeDisplay>
+      <ResetButton onPress={resetTimer}></ResetButton>
+      <StartButton onPress={startTick}></StartButton>
+      <StopButton onPress={stopTick}></StopButton>
     </View>
   );
+}
+
+function ResetButton({onPress}) {
+  return (
+    <Button title={"Reset"} onPress={onPress}></Button>
+  );
+}
+
+function StartButton({onPress}) {
+  return (
+    <Button
+      title={"Start"}
+      onPress={onPress}>
+    </Button>
+  )
+}
+
+function StopButton({onPress}) {
+  return (
+    <Button title={"Stop"} onPress={onPress}></Button>
+  )
 }
 
 function TimeDisplay({seconds}) {
@@ -28,7 +147,7 @@ function TimeDisplay({seconds}) {
 
   return (
     <Text style={styles.timeDisplay}>{timeString}</Text>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
